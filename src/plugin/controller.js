aa805e4f8bd57acca68389623ddb8efe693f3bb3
ZakharDay <zakhar.day@gmail.com>
2024-07-11 22:39:37 +0300

    Cleanup

--- src/plugin/controller.js
+++ src/plugin/controller.js
@@ -3,60 +3,6 @@ import { saveImageDataOrExportToFigma } from './images'
 figma.showUI(__html__)
 figma.ui.resize(668, 628)
 
-let colorModesToShow = []
-let colorVariablesToShow = []
-
-async function loadCollections() {
-  const localCollections =
-    await figma.variables.getLocalVariableCollectionsAsync()
-
-  localCollections.forEach((collection) => {
-    // need to save modes for later use
-    // modes: [
-    //   {
-    //     modeId: String,
-    //     name: String
-    //   }
-    // ]
-    loadCollection(collection.id)
-  })
-}
-
-async function loadCollection(id) {
-  const collection = await figma.variables.getVariableCollectionByIdAsync(id)
-  // console.log('collection', collection)
-
-  collection.modes.forEach((mode) => {
-    const { modeId, name } = mode
-
-    colorModesToShow.push({
-      modeId,
-      name
-    })
-  })
-
-  collection.variableIds.forEach((variableId) => {
-    loadVariable(variableId)
-  })
-}
-
-async function loadVariable(id) {
-  const variable = await figma.variables.getVariableByIdAsync(id)
-  // console.log('variable', variable, variable.name, variable.resolvedType)
-
-  const { resolvedType, name, valuesByMode } = await variable
-
-  if (resolvedType == 'COLOR') {
-    colorVariablesToShow.push({ name, valuesByMode })
-  } else {
-    console.log('VAR not color type')
-  }
-
-  // console.log('colorVariablesToShow', colorVariablesToShow)
-}
-
-loadCollections()
-
 figma.ui.onmessage = (msg) => {
   console.log('FIGMA JUST GOT A MESSAGE, YO', msg)
 
