d42dfd10fde79e5550927a7ec17b4b8476a4ea73
ntizish <dan.perednja@gmail.com>
2025-02-16 14:46:22 +0200

    First slide fully operational

--- src/libraries/templates.js
+++ src/libraries/templates.js
@@ -4,7 +4,7 @@ const templates = {
     titleSlide: {
       title: "Title Slide",
       elements: [
-        { type: "image", imageSrc: "https://picsum.photos/200", position: [0, 0], size: [1920, 1080] },
+        { type: "image", imageSrc: "https://raw.githubusercontent.com/ntizish/startup-starter-1/refs/heads/main/src/app/assets/images/slides/CM/CM_1.png", position: [0, 0], size: [1920, 1080] },
         { type: "text", content: "Project Title", position: [360, 440], color: '#000000', fontSize: 96, size: [1200, 200], alignHorizontal: "CENTER", alignVertical: "CENTER" },
         { type: "text", content: "Larger description of the way that product solves the issue. May be longer that a sentence and up to 8 lines of text.", position: [664, 870], color: '#000000', fontSize: 20, size: [592, 160], alignHorizontal: "CENTER", alignVertical: "BOTTOM"  }]
     }
