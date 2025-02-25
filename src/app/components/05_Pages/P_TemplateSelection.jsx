6a558e72b9319c9574bfaa1ca276cdda125491ec
ntizish <dan.perednja@gmail.com>
2025-02-25 23:18:52 +0200

    Button good

--- src/app/components/05_Pages/P_TemplateSelection.jsx
+++ src/app/components/05_Pages/P_TemplateSelection.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import A_Button from '../01_Atoms/A_Button';
 import M_GenerationNav from '../02_Molecules/M_GenerationNav';
 import { ArrowCircleRight } from '@phosphor-icons/react';
+import M_TemplateOption from '../02_Molecules/M_TemplateOption';
 
 export default function P_TemplateSelection({ onSelect, onBack, selectedTemplate, onTemplateSelect }) {
   return (
     <div className="P_TemplateSelection">
-        <M_GenerationNav onBack={onBack} />
+      <M_GenerationNav onBack={onBack} />
       
       <div className="template-options">
-        <button
-          className={`template-option ${selectedTemplate === 'CreativeMess' ? 'selected' : ''}`}
+        <M_TemplateOption
+          name="Creative Mess"
+          imageUrl="src/app/assets/images/slides/CM/preview.png"
+          tags={[
+            { text: "Creative", fill: true },
+            { text: "Modern", fill: false },
+            { text: "Dynamic", fill: false }
+          ]}
+          isSelected={selectedTemplate === 'CreativeMess'}
           onClick={() => {
             console.log('Template selected:', 'CreativeMess');
             onTemplateSelect('CreativeMess')
           }}
-        >
-          <span className="template-name">Creative Mess</span>
-          <span className="template-preview">Preview image here</span>
-        </button>
+        />
       </div>
+      
       <div className="navigation">
         <A_Button 
           onClick={onSelect} 
