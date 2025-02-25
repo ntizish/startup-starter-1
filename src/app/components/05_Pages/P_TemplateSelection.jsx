5f4db17072235d42e5c447f3c28d9af11d8b97ea
ntizish <dan.perednja@gmail.com>
2025-02-25 23:59:39 +0200

    Template selection step is in design now

--- src/app/components/05_Pages/P_TemplateSelection.jsx
+++ src/app/components/05_Pages/P_TemplateSelection.jsx
@@ -10,33 +10,37 @@ export default function P_TemplateSelection({ onSelect, onBack, selectedTemplate
     <div className="P_TemplateSelection">
       <M_GenerationNav onBack={onBack} />
       
-      <div className="M_StepIndicator">
-        <div className="A_StepDot"><div className="A_StepDot_Inner"></div></div>
-        <div className="A_StepDot"><div className="A_StepDot_Inner"></div></div>
-        <div className="A_StepDot A_StepDot--active"><div className="A_StepDot_Inner"></div></div>
-        <div className="A_StepDot"></div>
-      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '32px', alignItems: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', alignItems: 'center' }}>
+          <div className="M_StepIndicator">
+            <div className="A_StepDot A_StepDot--active"><div className="A_StepDot_Inner"></div></div>
+            <div className="A_StepDot"></div>
+            <div className="A_StepDot"></div>
+            <div className="A_StepDot"></div>
+          </div>
 
-      <h1 className="A_Text A_Text--heading">What graphics suit your project the best?</h1>
-      
-      <div className="template-options" style={{width: '100%'}}>
-        <M_TemplateOption
-          image={creativeMessPreview}
-          tags={[
-            { 
-              text: "Creative", 
-              backgroundColor: "#C7D6EB",
-              textColor: "#1E3D66"
-            },
-            { 
-              text: "Modern", 
-              backgroundColor: "#C1EEBB",
-              textColor: "#28661E"
-            }
-          ]}
-          isSelected={selectedTemplate === 'CreativeMess'}
-          onClick={() => onTemplateSelect('CreativeMess')}
-        />
+          <h1 className="A_Text A_Text--heading A_Text--heading-center" style={{width: '428px'}}>What graphics suit your project the best?</h1>
+        </div>
+        
+        <div className="template-options" style={{width: '100%'}}>
+          <M_TemplateOption
+            image={creativeMessPreview}
+            tags={[
+              { 
+                text: "Creative", 
+                backgroundColor: "#C7D6EB",
+                textColor: "#1E3D66"
+              },
+              { 
+                text: "Modern", 
+                backgroundColor: "#C1EEBB",
+                textColor: "#28661E"
+              }
+            ]}
+            isSelected={selectedTemplate === 'CreativeMess'}
+            onClick={() => onTemplateSelect('CreativeMess')}
+          />
+        </div>
       </div>
       
       <div className="navigation">
